e6bae0112f3bf26922136779e76a81af48aa7c21
Edward Barnard <ewbarnard@embarqmail.com>
2017-10-28 06:41:22 -0500

    Check "Perform code analysis" context.

--- bash/create-project.sh
+++ bash/create-project.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash -v
+# Installation instructions at https://book.cakephp.org/3.0/en/installation.html
+
 composer self-update
 yes | composer create-project --prefer-dist cakephp/app station
 composer require --dev dereuromark/cakephp-ide-helper
